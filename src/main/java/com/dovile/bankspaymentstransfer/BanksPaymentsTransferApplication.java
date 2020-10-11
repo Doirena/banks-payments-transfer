f4c7273112ef066d0375d8b80b0d385532be9422
Doreina <barkauskaite.dovile@gmail.com>
2020-10-11 12:06:00 +0300

    Create a new project

---
+++ src/main/java/com/dovile/bankspaymentstransfer/BanksPaymentsTransferApplication.java
@@ -0,0 +1,13 @@
+package com.dovile.bankspaymentstransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksPaymentsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksPaymentsTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dovile/bankspaymentstransfer/BanksPaymentsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dovile.bankspaymentstransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksPaymentsTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
