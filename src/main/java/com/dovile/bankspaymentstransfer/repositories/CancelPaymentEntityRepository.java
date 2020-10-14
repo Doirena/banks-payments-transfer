343d9b11585bc0f04890e864543b1207ff482531
Doreina <barkauskaite.dovile@gmail.com>
2020-10-14 15:03:51 +0300

    add query to find payment id

--- src/main/java/com/dovile/bankspaymentstransfer/repositories/CancelPaymentEntityRepository.java
+++ src/main/java/com/dovile/bankspaymentstransfer/repositories/CancelPaymentEntityRepository.java
@@ -2,8 +2,15 @@ package com.dovile.bankspaymentstransfer.repositories;
 
 import com.dovile.bankspaymentstransfer.entities.CancelPaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
 @Repository
 public interface CancelPaymentEntityRepository extends JpaRepository<CancelPaymentEntity, Integer> {
+    @Query(name = "CancelPaymentEntity.findPaymentID")
+    CancelPaymentEntity findPaymentID(@Param("id") Integer id);
 }
