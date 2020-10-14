6894357bfb3fcbac89e7e9a9369cba812e778d4c
Doreina <barkauskaite.dovile@gmail.com>
2020-10-14 15:10:07 +0300

    Create data.sql where input data for type table and currency table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO currency_data (name, rate) VALUES ('EUR', 1 );
 INSERT INTO currency_data (name, rate) VALUES ('USD', 0.8453 );
 
-INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.050 );
-INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.100 );
-INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.150 );
+INSERT INTO payment_type (type_name, coefficient) VALUES ('TYPE1', 0.050 );
+INSERT INTO payment_type (type_name, coefficient) VALUES ('TYPE2', 0.100 );
+INSERT INTO payment_type (type_name, coefficient) VALUES ('TYPE3', 0.150 );
 
 
 
