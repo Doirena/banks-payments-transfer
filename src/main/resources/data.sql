aef5ee9e1eb5d7132accf017c75f34ecdd38ae00
Doreina <barkauskaite.dovile@gmail.com>
2020-10-14 11:26:59 +0300

    Inssert data in H2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO currency_data (name, rate) VALUES ('EUR', 1 );
+INSERT INTO currency_data (name, rate) VALUES ('USD', 0.8453 );
+
+INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.050 );
+INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.100 );
+INSERT INTO payment_type (type_name, coefficient) VALUES ('EUR', 0.150 );
+
+
+
