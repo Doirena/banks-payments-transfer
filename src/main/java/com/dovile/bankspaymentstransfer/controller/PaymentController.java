f2b6d101b5f8d815e89ebdea2ff2bcfd87902f60
Doreina <barkauskaite.dovile@gmail.com>
2020-10-14 20:38:31 +0300

    Correct controller for Spring HATEOAS get links

--- src/main/java/com/dovile/bankspaymentstransfer/controller/PaymentController.java
+++ src/main/java/com/dovile/bankspaymentstransfer/controller/PaymentController.java
@@ -1,13 +1,19 @@
 package com.dovile.bankspaymentstransfer.controller;
 
+import com.dovile.bankspaymentstransfer.assemblers.CancelPaymentAssembler;
+import com.dovile.bankspaymentstransfer.assemblers.PaymentModelAssembler;
 import com.dovile.bankspaymentstransfer.domain.request.PaymentsRequest;
 import com.dovile.bankspaymentstransfer.domain.response.CancelPaymentResponse;
+import com.dovile.bankspaymentstransfer.domain.response.PaymentResponse;
 import com.dovile.bankspaymentstransfer.domain.response.PaymentsIdResponse;
-import com.dovile.bankspaymentstransfer.entities.CancelPaymentEntity;
-import com.dovile.bankspaymentstransfer.entities.PaymentsEntity;
 import com.dovile.bankspaymentstransfer.exceptions.BadInputException;
 import com.dovile.bankspaymentstransfer.exceptions.ResourceNotFoundException;
+import com.dovile.bankspaymentstransfer.assemblers.PaymentsIdModelAssembler;
 import com.dovile.bankspaymentstransfer.services.PaymentService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,43 +24,61 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
  */
 @RestController
 @Validated
-public class PaymentController{
+public class PaymentController {
 
     private final PaymentService paymentService;
+    private final PaymentsIdModelAssembler paymentsAssembler;
+    private final CancelPaymentAssembler cancelPaymentAssembler;
+    private final PaymentModelAssembler paymentModelAssembler;
 
-
-    PaymentController(PaymentService paymentService) {
+    PaymentController(PaymentService paymentService,PaymentsIdModelAssembler paymentsAssembler,
+                      CancelPaymentAssembler cancelPaymentAssembler, PaymentModelAssembler paymentModelAssembler) {
         this.paymentService = paymentService;
+        this.paymentsAssembler = paymentsAssembler;
+        this.cancelPaymentAssembler = cancelPaymentAssembler;
+        this.paymentModelAssembler = paymentModelAssembler;
     }
 
     @PostMapping(value = "/createPayment", params = {"type", "currency"})
-    public PaymentsEntity createPayment(
+    public ResponseEntity<EntityModel<PaymentResponse>> createPayment(
             @Valid @RequestBody PaymentsRequest request,
             @RequestParam(value = "type") String type,
             @RequestParam(value = "currency") String currency) throws ResourceNotFoundException, BadInputException {
-        return paymentService.createPayment(request,type,currency);
+        EntityModel<PaymentResponse> entityModel = paymentModelAssembler.toModel(
+                paymentService.createPayment(request, type, currency));
+        return ResponseEntity.ok(entityModel);
     }
 
     @PostMapping("cancelPayment/{id}")
-    public CancelPaymentEntity cancelPayment(
-            @PathVariable(value = "id") Integer id)throws ResourceNotFoundException {
-        return paymentService.cancelPayment(id);
+    public ResponseEntity<EntityModel<CancelPaymentResponse>> cancelPayment(
+            @PathVariable(value = "id") Integer id) throws ResourceNotFoundException {
+        EntityModel<CancelPaymentResponse> entityModel = cancelPaymentAssembler.toModel(paymentService.cancelPayment(id));
+        return ResponseEntity.ok(entityModel);
     }
 
-        @GetMapping("payments")
-    public List<PaymentsIdResponse> getAllExistPayments() {
-        return paymentService.getAllPaymentsId();
+    @GetMapping("payments")
+    public CollectionModel<EntityModel<PaymentsIdResponse>> getAllExistPayments() {
+        List<EntityModel<PaymentsIdResponse>> employees = paymentService.getAllPaymentsId().stream()
+                .map(paymentsAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees,
+                linkTo(methodOn(PaymentController.class).getAllExistPayments()).withSelfRel());
     }
 
     @GetMapping("cancelPaymentFee/{id}")
-    public CancelPaymentResponse getAllCancelPayments(
+    public EntityModel<CancelPaymentResponse> getCancelPaymentById(
             @PathVariable(value = "id") Integer id) throws ResourceNotFoundException {
-        return paymentService.getCancelPaymentById(id);
+        CancelPaymentResponse cancelPaymentResponse = paymentService.getCancelPaymentById(id);
+        return cancelPaymentAssembler.toModel(cancelPaymentResponse);
     }
 }
