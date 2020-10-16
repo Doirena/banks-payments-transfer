8480196eb84215a67bc0aee0b5dc8fb50e0d0a0f
Doreina <barkauskaite.dovile@gmail.com>
2020-10-17 02:22:49 +0300

    Correct the iban valid method

--- src/main/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckIbanValidation.java
+++ src/main/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckIbanValidation.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- *
  * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
  */
 public class CheckIbanValidation {
@@ -17,17 +16,15 @@ public class CheckIbanValidation {
     private final int IBANNUMBER_MAX_SIZE = 34;
 
     //Return true or false of IBAN
-    public boolean isValid(String iban) {
-        if (iban == null){
-            return false;
+    public boolean isIBANValid(String iban) {
+        if (iban != null) {
+            iban = iban.replaceAll("\\s", "").toUpperCase(Locale.ROOT);
+            if (checkIbanLenght(iban)) {
+                BigInteger integerValue = stringConvertToInteger(iban);
+                return integerValue.mod(BIG_INTEGER_97).intValue() == EXPECTED_MOD_97;
+            }
         }
-        iban = iban.replaceAll("\\s", "").toUpperCase(Locale.ROOT);
-        if (!checkIbanLenght(iban)) {
-            return false;
-        }else{
-            BigInteger integerValue = stringConvertToInteger(iban);
-            return integerValue.mod(BIG_INTEGER_97).intValue() == EXPECTED_MOD_97;
-        }
+        return false;
     }
 
     //1. Construct new IBAN no, Country code and chars move to the end
@@ -36,6 +33,7 @@ public class CheckIbanValidation {
         String newIban = iban.substring(4) + iban.substring(0, 4);
         return new BigInteger(replaceCountryCharactersWithDigits(newIban));
     }
+
     //Make IBAN no full digits
     private String replaceCountryCharactersWithDigits(String iban) {
         StringBuilder sb = new StringBuilder();
@@ -44,6 +42,7 @@ public class CheckIbanValidation {
         }
         return sb.toString();
     }
+
     //Check IBAN lenght if its correct by some criteria
     private boolean checkIbanLenght(String iban) {
         Map<String, Integer> CountryCodeDigitSum = new HashMap<>();
@@ -51,7 +50,7 @@ public class CheckIbanValidation {
         for (String countryChars : new Countrychars().getCOUNTRY_CHARS()) {
             CountryCodeDigitSum.put(countryChars.substring(0, 2), Integer.parseInt(countryChars.substring(2)));
         }
-        if (iban.length()>IBANNUMBER_MAX_SIZE
+        if (iban.length() > IBANNUMBER_MAX_SIZE
                 || iban.length() < IBANNUMBER_MIN_SIZE
                 || !iban.matches("[0-9A-Z]+")
                 || CountryCodeDigitSum.getOrDefault(iban.substring(0, 2), 0) != iban.length()) {
