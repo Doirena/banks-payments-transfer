24332a2cab5a93547bbc2d6202f3ce12417a1e46
Doreina <barkauskaite.dovile@gmail.com>
2020-10-18 01:58:42 +0300

    Corret method createPayment change valid. All fiekd validation is moved to other class and call validation in controller classes Also add comments

--- src/main/java/com/dovile/bankspaymentstransfer/services/PaymentServiceImpl.java
+++ src/main/java/com/dovile/bankspaymentstransfer/services/PaymentServiceImpl.java
@@ -14,16 +14,15 @@ import com.dovile.bankspaymentstransfer.repositories.CancelPaymentEntityReposito
 import com.dovile.bankspaymentstransfer.repositories.CurrencyDataEntityRepository;
 import com.dovile.bankspaymentstransfer.repositories.PaymentTypeEntityRepository;
 import com.dovile.bankspaymentstransfer.repositories.PaymentsEntityRepository;
-import com.dovile.bankspaymentstransfer.validator.checkiban_biccodes.CheckBicCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 /**
@@ -33,6 +32,8 @@ import java.util.logging.Logger;
 public class PaymentServiceImpl implements PaymentService {
 
     private final static Logger logger = Logger.getLogger(PaymentServiceImpl.class.getName());
+
+    private final long HOURS = 60 * 60 * 1000;
 
     @Autowired
     PaymentsEntityRepository paymentsEntityRepository;
@@ -46,85 +47,83 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     CancelPaymentEntityRepository cancelPaymentEntityRepository;
 
+    /**
+     *
+     * @param paymentsRequest
+     * @param type
+     * @param currency
+     * @return new payment
+     * @throws ResourceNotFoundException
+     * @throws BadInputException
+     */
     @Override
-    public PaymentResponse createPayment(PaymentsRequest request, String type, String currency) throws ResourceNotFoundException, BadInputException {
+    public PaymentResponse createPayment(PaymentsRequest paymentsRequest, String type, String currency)
+            throws ResourceNotFoundException, BadInputException {
+        type = type.replaceAll("\\s", "").toUpperCase(Locale.ROOT);
+        currency = currency.replaceAll("\\s", "").toUpperCase(Locale.ROOT);
         PaymentsEntity paymentsEntity = new PaymentsEntity();
-        PaymentTypeEntity paymentTypeEntity = paymentTypeEntityRepository.findByTypeName(type);
-        if (paymentTypeEntity != null) {
-            paymentsEntity.setPaymentType(paymentTypeEntity);
-        } else {
-            logger.warning("Bad TYPE");
-            throw new ResourceNotFoundException("Please insert a correct Type");
-        }
 
-        CurrencyDataEntity currencyDataEntity = currencyDataEntityRepository.findByName(currency);
-        if (currencyDataEntity != null) {
-            paymentsEntity.setCurrencyData(currencyDataEntity);
-            if (type.equals("TYPE1") && currency.equals("EUR")) {
-                paymentsEntity.setCurrencyData(currencyDataEntity);
-                if (request.getAdditionalField() == null) {
-                    throw new BadInputException("Please enter Addition field");
-                } else {
-                    paymentsEntity.setAdditionalField(request.getAdditionalField());
-                }
-            } else if (type.equals("TYPE2") && currency.equals("USD")) {
-                paymentsEntity.setCurrencyData(currencyDataEntity);
-                paymentsEntity.setAdditionalField(request.getAdditionalField());
-            } else if (type.equals("TYPE3")) {
-                paymentsEntity.setCurrencyData(currencyDataEntity);
-                if (!(new CheckBicCode().isValidBicCode(request.getAdditionalField()))) {
-                    throw new BadInputException("Please enter right Bic Code");
-                } else {
-                    paymentsEntity.setAdditionalField(request.getAdditionalField());
-                }
-            } else {
-                throw new BadInputException("Bad currency type");
-            }
-        } else {
-            logger.warning("Bad Currency");
-            throw new ResourceNotFoundException("Please insert a correct Currency");
-        }
-        paymentsEntity.setAmount(request.getAmount());
-        paymentsEntity.setDebtorIban(request.getDebtorIban());
-        paymentsEntity.setCreditorIban(request.getCreditorIban());
-        PaymentsEntity newPayment = paymentsEntityRepository.save(paymentsEntity);
+        PaymentTypeEntity paymentTypeEntity = paymentTypeEntityRepository.findByTypeName(type).
+                orElseThrow(() -> new ResourceNotFoundException("Please insert a correct Type"));
+
+        paymentsEntity.setPaymentType(paymentTypeEntity);
+
+        CurrencyDataEntity currencyDataEntity = currencyDataEntityRepository.findByName(currency).
+                orElseThrow(() -> new ResourceNotFoundException("Please insert a correct Currency"));
+
+        paymentsEntity.setCurrencyData(currencyDataEntity);
+        paymentsEntity.setDetails(paymentsRequest.getDetails());
+        paymentsEntity.setBic_code(paymentsRequest.getBic_code());
+        paymentsEntity.setAmount(Double.parseDouble(paymentsRequest.getAmount()));
+        paymentsEntity.setDebtorIban(paymentsRequest.getDebtorIban());
+        paymentsEntity.setCreditorIban(paymentsRequest.getCreditorIban());
+        paymentsEntityRepository.save(paymentsEntity);
         logger.info("Create new payment");
-        return new PaymentResponse(newPayment.getId(), newPayment.getAmount(),
-                newPayment.getDebtorIban(), newPayment.getCreditorIban(), newPayment.getAdditionalField());
+        return new PaymentResponse(paymentsEntity.getId(), paymentsEntity.getAmount(),
+                paymentsEntity.getDebtorIban(), paymentsEntity.getCreditorIban(),
+                paymentsEntity.getDetails(), paymentsEntity.getBic_code());
     }
 
+    /**
+     *
+     * @param paymentId
+     * @return new Canceled payment with canceled fee.
+     * @throws ResourceNotFoundException
+     * @throws BadInputException
+     */
     @Transactional
     @Override
-    public CancelPaymentResponse cancelPayment(Integer id) throws ResourceNotFoundException {
-        PaymentsEntity paymentsEntity = paymentsEntityRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Payment not found on: " + id));
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date realDate = new Date();
+    public CancelPaymentResponse cancelPayment(Integer paymentId) throws ResourceNotFoundException, BadInputException {
+        PaymentsEntity paymentsEntity = paymentsEntityRepository.findById(paymentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Payment not found on: " + paymentId));
 
         //If state is false
         if (!paymentsEntity.getStatus()) {
-            throw new ResourceNotFoundException("You can't cancel the payment is canceleted");
+            throw new BadInputException("You can't cancel the payment is canceleted");
         }
         //first check time
-        if (format.format(paymentsEntity.getPaymentDate()).compareTo(format.format(realDate)) < 0) {
-            throw new ResourceNotFoundException("You can't cancel the payment time is off");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        if (format.format(paymentsEntity.getPaymentDate()).compareTo(format.format(new Date())) < 0) {
+            throw new BadInputException("You can't cancel the payment time is off");
         }
-        //calculate hours and cancelfee
-        long diff = (realDate.getTime() - paymentsEntity.getPaymentDate().getTime()) / (60 * 60 * 1000) % 24;
-        BigDecimal fee = new BigDecimal(diff).multiply(paymentsEntity.getPaymentType().getCoefficient());
+
         CancelPaymentEntity cancelPaymentEntity = new CancelPaymentEntity();
         cancelPaymentEntity.setPayments(paymentsEntity);
-        cancelPaymentEntity.setCancelFee(fee);
+        cancelPaymentEntity.setCancelFee(
+                calcCancelFee(paymentsEntity.getPaymentDate(), paymentsEntity.getPaymentType().getCoefficient()));
 
         //Cancel payment create and update payment status;
         paymentsEntity.setStatus(false);
         paymentsEntityRepository.save(paymentsEntity);
-        CancelPaymentEntity updateCancel = cancelPaymentEntityRepository.save(cancelPaymentEntity);
+        cancelPaymentEntityRepository.save(cancelPaymentEntity);
         logger.info("Create cancel payment");
-        return new CancelPaymentResponse(updateCancel.getPayments().getId(), updateCancel.getCancelFee());
+        return new CancelPaymentResponse(cancelPaymentEntity.getPayments().getId(), cancelPaymentEntity.getCancelFee());
     }
 
+    /**
+     *
+     * @return list of active payments. Returns just payment id.
+     */
     @Override
     public List<PaymentsIdResponse> getAllPaymentsId() {
         List<PaymentsIdResponse> paymentsIdResponseList = new ArrayList<>();
@@ -134,13 +133,30 @@ public class PaymentServiceImpl implements PaymentService {
         return paymentsIdResponseList;
     }
 
+    /**
+     *
+     * @param paymentId
+     * @return canceled payment by payments id. Returns value: payments id and canceled fee
+     * @throws ResourceNotFoundException
+     */
     @Override
-    public CancelPaymentResponse getCancelPaymentById(Integer id) throws ResourceNotFoundException {
-        CancelPaymentEntity c = cancelPaymentEntityRepository.findPaymentID(id);
-        if (c == null) {
-            throw new ResourceNotFoundException("There isn't cancel this payment");
-        }
+    public CancelPaymentResponse getCancelPaymentById(Integer paymentId) throws ResourceNotFoundException {
+        CancelPaymentEntity c = cancelPaymentEntityRepository.findPaymentID(paymentId).
+                orElseThrow(() -> new ResourceNotFoundException("There isn't cancel this payment"));
         return new CancelPaymentResponse(c.getPayments().getId(), c.getCancelFee());
     }
 
+    /**
+     *
+     * @param paymentDate
+     * @param coefficient
+     * @return Cancel payment fee, which is get by formula: h*k
+     * k is coefficient, which we get from Payment Type table. It is by default according to each type;
+     * h is hours, which get calculate diff between payment create time and real time.
+     */
+    private Double calcCancelFee(Date paymentDate, Double coefficient){
+        long diff = (new Date().getTime() - paymentDate.getTime()) / HOURS % 24;
+        Double fee = (double)(diff)*coefficient;
+        return fee;
+    }
 }
