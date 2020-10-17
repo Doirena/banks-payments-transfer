aaf03a65d720476ec0dfb942b9b1201f0d99be53
Doreina <barkauskaite.dovile@gmail.com>
2020-10-18 00:15:58 +0300

    Delete what is not used and correct commits

--- src/main/java/com/dovile/bankspaymentstransfer/exceptions/BadInputException.java
+++ src/main/java/com/dovile/bankspaymentstransfer/exceptions/BadInputException.java
@@ -3,6 +3,9 @@ package com.dovile.bankspaymentstransfer.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * @author Dovile Barkauskaite <barkauskaite.dovile@gmail.com>
+ */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BadInputException extends Exception {
 
--- src/main/java/com/dovile/bankspaymentstransfer/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/dovile/bankspaymentstransfer/exceptions/ResourceNotFoundException.java
@@ -14,4 +14,5 @@ public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String message) {
         super(message);
     }
+
 }
