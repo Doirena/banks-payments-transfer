475bea1704df22f3638f89721e402a2edac20ae7
Doreina <barkauskaite.dovile@gmail.com>
2020-10-18 22:32:54 +0300

    correct test (iban and bic code) methods name

--- src/test/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckBicCodeTest.java
+++ src/test/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckBicCodeTest.java
@@ -7,14 +7,14 @@ import static org.junit.Assert.*;
 public class CheckBicCodeTest {
 
     @Test
-    public void isBICCodeValid_true() {
+    public void give_Right_Bic_code_Should_Return_true() {
         CheckBicCode bicCode = new CheckBicCode();
         Boolean isValid = bicCode.isBICCodeValid("KUSRLT24");
         assertEquals(true, isValid );
     }
 
     @Test
-    public void isBICCodeValid_false() {
+    public void give_Bad_Bic_code_Should_Return_false() {
         CheckBicCode bicCode = new CheckBicCode();
         Boolean isValid = bicCode.isBICCodeValid("KU-SRLT24");
         assertEquals(false, isValid );
--- src/test/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckIbanValidationTest.java
+++ src/test/java/com/dovile/bankspaymentstransfer/validator/checkiban_biccodes/CheckIbanValidationTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.*;
 public class CheckIbanValidationTest {
 
     @Test
-    public void isIBANValid_true() {
+    public void give_Right_Iban_Should_Return_true() {
         CheckIbanValidation iban = new CheckIbanValidation();
         Boolean result = iban.isIBANValid("LT647044001231465456");
         assertEquals(true, result);
     }
     @Test
-    public void isIBANValid_false() {
+    public void give_Bad_Iban_Should_Return_false() {
         CheckIbanValidation iban = new CheckIbanValidation();
         Boolean result = iban.isIBANValid("BBBBBB");
         assertEquals(false, result);
